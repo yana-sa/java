1a5a8c0a97693b4a2be9f83eecc1a868dfaf3fdc
yana.sam <yana.samahala@agiliway.com>
2023-07-10 11:45:26 +0300

    remove comments

--- HelloWorldServlet/src/main/java/HelloWorld.java
+++ HelloWorldServlet/src/main/java/HelloWorld.java
@@ -8,23 +8,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorld
- */
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public HelloWorld() {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		
@@ -32,25 +23,16 @@ public class HelloWorld extends HttpServlet {
 		out.println("Hello World!");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		PrintWriter out = response.getWriter();
 		out.println("POST request");
 	}
 
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("PUT request");
 	}
-	
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("DELETE request");
